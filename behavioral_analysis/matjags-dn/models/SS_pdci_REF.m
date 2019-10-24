5dba7d54ff736ed32373bf01cadfcfa47cdf9f93
kbonna <kongokou@gmail.com>
2019-10-24 15:50:21 +0200

    refactored ba notebooks, added paramer recovery analysis

---
+++ behavioral_analysis/matjags-dn/models/SS_pdci_REF.m
@@ -0,0 +1,122 @@
+clear; clc;
+
+%% Setup
+% task data location
+root = '/home/kmb/Desktop/Neuroscience/Projects/BONNA_decide_net/data/main_fmri_study/sourcedata/behavioral/';
+root_resp_artif = '/home/kmb/Desktop/Neuroscience/Projects/BONNA_decide_net/code/behavioral_analysis/matjags-dn/data/parameter_recovery/';
+fname_beh = 'behavioral_data_clean_all_REF.mat';
+fname_meta = 'behavioral_data_clean_all_REF.json';
+   
+% load behavioral and metadata
+load(strcat(root, fname_beh));
+fid = fopen(strcat(root, fname_meta)); 
+raw = fread(fid, inf); 
+str = char(raw'); 
+fclose(fid); 
+meta = jsondecode(str);
+clearvars -except beh meta root_resp_artif
+
+nSubjects = numel(meta.dim1);
+nConditions = numel(meta.dim2);
+nTrials = numel(meta.dim3);
+nPredErrSign = 2;
+
+% Parameters
+fname_model = fullfile(pwd, strcat(mfilename, '.txt'));
+doparallel = 1;         % parallelization flag
+thinning = 1;           % thinning parameter
+nChains = 4;                                        
+nBurnin = 50;
+nSamples = 250;
+
+% Initialize Markov chain values
+for i=1:nChains
+    S = struct;         % random initial values
+    init0(i) = S;
+end
+
+monitorparams = {'alpha_pdci', 'beta_pdci'};
+
+%% Loop over parameter space
+nTaskSamples = 5;   % number of different task realizations
+bt = 1;             % fixed inverse-temperature
+
+samples_ap = zeros(21, 21, nChains*nSamples, nTaskSamples);
+samples_am = zeros(21, 21, nChains*nSamples, nTaskSamples);
+samples_bt = zeros(21, 21, nChains*nSamples, nTaskSamples);
+
+tic 
+counter = -1;
+for ap = 1 : 21
+    for am = 1 : 21
+        
+        % Generate information for the user
+        time = toc;
+        counter = counter + 1;
+        time_left_minutes = ((441/counter) - 1) * time / 60;
+        fprintf('Progress: %.2f %% (%i/441 models estimated)\n', 100*counter/441, counter);
+        fprintf('Estimated time to go: %ih %imin\n', ...
+            floor(time_left_minutes/60), ... 
+            floor(mod(time_left_minutes, 60)));
+
+        % Draw random five task realizations
+        subjects_idx = 1:32;
+        subjects_idx = subjects_idx(randperm(length(subjects_idx)));
+        subjects_idx = subjects_idx(1:nTaskSamples);
+
+        for i = 1 : nTaskSamples 
+
+            s = subjects_idx(i);
+
+            % artificial response data location
+            fname_resp_artif = strcat( ...
+                root_resp_artif, ...
+                strcat('response_synthetic_sub-', meta.dim1{s}, '.mat'));
+            load(fname_resp_artif);
+
+            % pool task data & synthetic response data
+            xl = squeeze(beh(s, :, :, strcmp(meta.dim4, 'magn_left')));     % reward magnitude for left box
+            xr = squeeze(beh(s, :, :, strcmp(meta.dim4, 'magn_right')));    % reward magnitude for right box
+            side = squeeze(beh(s, :, :, strcmp(meta.dim4, 'side')));        % correct side
+            resp = squeeze(response_synthetic(ap, am, bt, :, :));           % synthetic response
+
+            % minor preprocessing
+            resp(resp == 0) = NaN;              % set missing values to NaNs
+            resp = (resp + 1) / 2;              % 0: left box; 1: right box; NaN: miss
+            side = (side + 1) / 2;              % 0: left box; 1: right box (correct)
+
+            % Assign MATLAB variables to the observed JAGS nodes
+            datastruct = struct(...
+                'xl', xl, ...
+                'xr', xr, ...
+                'side', side, ...
+                'resp', resp, ...
+                'nConditions', nConditions, ...
+                'nPredErrSign', nPredErrSign, ...
+                'nTrials', nTrials);
+
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % RUN JAGS
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            [samples, stats, structArray] = matjags( ...
+                datastruct, ...                     % Observed data
+                fname_model, ...                    % File that contains model definition
+                init0, ...
+                'doparallel' , doparallel, ...      % Parallelization flag
+                'nchains', nChains,...              % Number of MCMC chains
+                'nburnin', nBurnin,...              % Number of burnin steps
+                'nsamples', nSamples, ...           % Number of samples to extract
+                'thin', thinning, ...               % Thinning parameter
+                'monitorparams', monitorparams, ... % List of latent variables to monitor
+                'savejagsoutput', 0 , ...           % Save command line output produced by JAGS?
+                'verbosity', 0 , ...                % 0=do not produce any output; 1=minimal text output; 2=maximum text output
+                'cleanup', 1 );                     % clean up of temporary files?
+
+            % Write outputs
+            samples_ap(ap, am, :, i) = reshape(samples.alpha_pdci(:,:,1), 1, []);
+            samples_am(ap, am, :, i) = reshape(samples.alpha_pdci(:,:,2), 1, []);
+            samples_bt(ap, am, :, i) = reshape(samples.beta_pdci, 1, []);
+        end
+        
+    end
+end
--- dn_utils/behavioral_models_REF.py
+++ dn_utils/behavioral_models_REF.py
@@ -36,10 +36,10 @@ def load_behavioral_data(root: str, verbose=True) -> tuple:
         
     return beh, meta
 
-
-### <--- REFACTORING POINT
-def estimate_values(beh, meta, subject, condition, alpha):
-    '''Implements TD learning model on experienced probabilistic outcomes.
+def estimate_wbci(beh, meta, subject, condition, alpha):
+    '''Implements TD learning model on side probabilities. Note that estimated 
+    probability is the probability that side will be chosen (rewarded in 
+    reward-seeking condition or punished in punishment-avoidance condition)
 
     Args:
         beh (np.array): aggregated behavioral responses
@@ -49,20 +49,156 @@ def estimate_values(beh, meta, subject, condition, alpha):
         alpha (float): learning rate
 
     Returns:
-        val (np.array): reflects algorithm trialwise beliefs about
-            probabilities that box will be rewarded / punished
+        np.array (n_trials x n_sides): 
+            reflects algorithm trialwise beliefs about probabilities that side 
+            will be chosen (rewarded / punished)
     '''
 
-    val = np.zeros((beh.shape[2], 2))
-    val[0] = [.5, .5] # Initial beliefs (agnostic)
+    wbci = np.zeros((beh.shape[2], 2))
+    wbci[0] = [.5, .5] # Initial beliefs (agnostic)
 
-    rewarded = np.copy(beh[subject, condition, :, meta['dim4'].index('rwd')][:-1])
+    side_bci = np.copy(
+        beh[subject, condition, :, meta['dim4'].index('side_bci')][:-1]
+    )
 
-    for trial, rwd in enumerate(rewarded):
-        val[trial+1, 1] = val[trial, 1] + alpha * ((rwd + 1)/2 - val[trial, 1])
-        val[trial+1, 0] = val[trial, 0] + alpha * ((-rwd + 1)/2 - val[trial, 0])
+    for t, rbci in enumerate(side_bci):
+        wbci[t+1, 1] = wbci[t, 1] + alpha * ((rbci + 1)/2 - wbci[t, 1])
+        wbci[t+1, 0] = wbci[t, 0] + alpha * ((-rbci + 1)/2 - wbci[t, 0])
 
-    return val
+    return wbci
+
+def estimate_util(beh, meta, subject, condition, gamma=1, delta=1):
+    '''Implements function converting reward magnitude to experienced utility.
+
+    Args:
+        beh (np.array): aggregated behavioral responses
+        meta (dict): description of beh array coding
+        subject (int): subject index
+        condition (int): task condition index
+        gamma (float): loss aversion parameter
+        delta: (float): risk aversion parameter
+
+    Returns:
+        np.array (n_trials x n_sides): 
+            reflects algorithm trialwise estimates of utility for both left and 
+            right boxes
+    '''
+    util = np.zeros((beh.shape[2], 2))
+
+    if condition == meta['dim2'].index('pun'):
+        factor = (-1) * gamma
+    else:
+        factor = 1
+
+    util[:, 0] = factor * np.power(
+        np.abs(beh[subject, condition, :, meta['dim4'].index('magn_left')]),
+        delta
+    )
+    util[:, 1] = factor * np.power(
+        np.abs(beh[subject, condition, :, meta['dim4'].index('magn_right')]),
+        delta
+    )
+
+    return util
+
+def estimate_choice_probability(wbci, util, kind='simple', beta=None):
+    '''Implements softmax decision rule reflecting choice probabilities
+
+    Args:
+        wbci (np.array): trialwise beliefs about probabilities that side will
+            be rewarded / punished
+        util (np.array): trialwise estimates of utility for both sides
+        kind (str): either 'simple' or 'softmax'
+        beta (float): inverse temperature for softmax function
+
+    Returns:
+        np.array (n_trials x n_sides): 
+            trialwise choice probabilities
+    '''
+
+    # Calculate expected value for both options
+    exvl = np.multiply(util, wbci)
+
+    if kind == 'simple':
+        prob = exvl / np.sum(exvl, axis=1)[:, np.newaxis]
+        if np.sum(exvl) < 0:
+            prob = np.fliplr(prob)
+
+    elif kind == 'softmax':
+        prob = np.exp(beta * exvl) / np.sum(np.exp(beta * exvl), axis=1)[:, np.newaxis]
+
+    return prob
+
+def g_square(beh, meta, subject, condition, prob):
+    '''Calculate badness-of-fit quality measure. G-square is inversely
+    related to log likelyhood.
+
+    Args:
+        beh (np.array): aggregated behavioral responses
+        meta (dict): description of beh array coding
+        subject (int): subject index
+        condition (int): task condition index
+        prob (np.array): trialwise choice probabilities
+
+    Returns:
+        (float): g-square badness-of-fit
+    '''
+
+    ll = 0
+    responses = beh[subject, condition, :, meta['dim4'].index('response')]
+
+    for trial, response in enumerate(responses):
+        if response == -1:
+            if prob[trial, 0] > 0: 
+                ll += np.log(prob[trial, 0])
+            else:
+                ll += np.log(np.finfo(float).eps)
+        elif response == 1:
+            if prob[trial, 1] > 0:
+                ll += np.log(prob[trial, 1])
+            else:
+                ll += np.log(np.finfo(float).eps)
+
+    return (-2) * ll
+
+
+### Full Models ################################################################
+def model1(beh, meta, subject, condition, alpha):
+    '''Simple one-parameter model with variable learning rate.'''
+
+    wbci = estimate_wbci(beh, meta, subject, condition, alpha)
+    util = estimate_util(beh, meta, subject, condition)
+    prob = estimate_choice_probability(wbci, util, kind='simple')
+
+    return (wbci, util, prob)
+
+
+def model2(beh, meta, subject, condition, alpha, beta):
+    '''Two-parameter model  with variable learning rate and inverse T.'''
+
+    wbci = estimate_wbci(beh, meta, subject, condition, alpha)
+    util = estimate_util(beh, meta, subject, condition)
+    prob = estimate_choice_probability(wbci, util, kind='softmax', beta=beta)
+
+    return (wbci, util, prob)
+
+def model3(beh, meta, subject, condition, alpha, beta, gamma, delta):
+    '''Four-parameter model.
+
+    Args:
+        alpha (float): learning rate
+        beta (float): inverse softmax temperature
+        gamma (float): loss aversion
+        delta (float): risk aversion
+    '''
+
+    wbci = estimate_wbci(beh, meta, subject, condition, alpha)
+    util = estimate_util(beh, meta, subject, condition, gamma, delta)
+    prob = estimate_choice_probability(wbci, util, kind='softmax', beta=beta)
+
+    return (wbci, util, prob)
+
+### <--- REFACTORING POINT ####################################################
 
 def estimate_values_pd(beh, meta, subject, condition, alpha_plus, alpha_minus):
     '''Implements TD learning model with separate learning rates for positive 
@@ -143,124 +279,5 @@ def estimate_regressors(beh, meta, subject, condition, val):
 
     return anticip_pwin, anticip_rew, pred_err
 
-def estimate_utilities(beh, meta, subject, condition, gamma=1, delta=1):
-    '''Implements function converting reward magnitude to experienced utility.
-
-    Args:
-        beh (np.array): aggregated behavioral responses
-        meta (dict): description of beh array coding
-        subject (int): subject index
-        condition (int): task condition index
-        gamma (float): loss aversion parameter
-        delta: (float): risk aversion parameter
-
-    Returns:
-        util (np.array): reflects algorithm trialwise estimates of utility
-            for both left and right boxes
-    '''
-    util = np.zeros((beh.shape[2], 2))
-
-    if condition == meta['dim2'].index('pun'):
-        factor = (-1) * gamma
-    else:
-        factor = 1
-
-    util[:, 0] = factor * np.power(
-        np.abs(beh[subject, condition, :, meta['dim4'].index('magn_left')]),
-        delta
-    )
-    util[:, 1] = factor * np.power(
-        np.abs(beh[subject, condition, :, meta['dim4'].index('magn_right')]),
-        delta
-    )
-
-    return util
-
-def estimate_choice_probability(val, util, kind='simple', theta=None):
-    '''Implements softmax decision rule reflecting choice probabilities
-
-    Args:
-        val (np.array): trialwise beliefs about probabilities that box will
-            be rewarded / punished
-        util (np.array): trialwise estimates of utility for both boxes
-        kind (str): either 'simple' or 'softmax' for two different models
-        theta (float): inverse temperature for softmax function
-
-    Returns:
-        p (np.array): trialwise choice probabilities
-    '''
-
-    # Calculate expected value for both options
-    ev = np.multiply(util, val)
-
-    if kind == 'simple':
-        p = ev / np.sum(ev, axis=1)[:, np.newaxis]
-        if np.sum(ev) < 0:
-            p = np.fliplr(p)
-
-    elif kind == 'softmax':
-        p = np.exp(theta * ev) / np.sum(np.exp(theta * ev), axis=1)[:, np.newaxis]
-
-    return p
-
-def g_square(beh, meta, subject, condition, p):
-    '''Calculate badness-of-fit quality measure. G-square is inversely
-    related to log likelyhood.
-
-    Args:
-        beh (np.array): aggregated behavioral responses
-        meta (dict): description of beh array coding
-        subject (int): subject index
-        condition (int): task condition index
-        p (np.array): trialwise choice probabilities
-
-    Returns:
-        (float): g-square badness-of-fit
-    '''
-
-    ll = 0
-    responses = beh[subject, condition, :, meta['dim4'].index('response')]
-
-    for trial, response in enumerate(responses):
-        if response == -1:
-            ll += np.log(p[trial, 0])
-        elif response == 1:
-            ll += np.log(p[trial, 1])
-
-    return (-2) * ll
-
-### Behavioral Models #######################################################
-def model1(beh, meta, subject, condition, alpha):
-    '''Simple one-parameter model with variable learning rate.'''
-
-    val = estimate_values(beh, meta, subject, condition, alpha)
-    util = estimate_utilities(beh, meta, subject, condition)
-    p = estimate_choice_probability(val, util, kind='simple')
-
-    return (val, util, p)
 
 
-def model2(beh, meta, subject, condition, alpha, theta):
-    '''Two-parameter model  with variable learning rate and inverse T.'''
-
-    val = estimate_values(beh, meta, subject, condition, alpha)
-    util = estimate_utilities(beh, meta, subject, condition)
-    p = estimate_choice_probability(val, util, kind='softmax', theta=theta)
-
-    return (val, util, p)
-
-def model3(beh, meta, subject, condition, alpha, theta, gamma, delta):
-    '''Four-parameter model.
-
-    Args:
-        alpha (float): learning rate
-        theta (float): inverse softmax temperature
-        gamma (float): loss aversion
-        delta (float): risk aversion
-    '''
-
-    val = estimate_values(beh, meta, subject, condition, alpha)
-    util = estimate_utilities(beh, meta, subject, condition, gamma, delta)
-    p = estimate_choice_probability(val, util, kind='softmax', theta=theta)
-
-    return (val, util, p)
